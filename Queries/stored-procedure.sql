faa7ccf0e6b4d69b88c163562651da680ba76d89
Aman Bhattarai <41770152+invictusaman@users.noreply.github.com>
2024-08-29 15:32:58 -0400

    Update stored-procedure.sql

--- Queries/stored-procedure.sql
+++ Queries/stored-procedure.sql
@@ -1,3 +1,5 @@
+-- The icon fields are calculated based on the value in the corresponding score field. You need to make sure that when a score field is updated, the icon field is updated too. To do this, you will write a stored procedure that receives the school id and a leaders score as input parameters, calculates the icon setting and updates the fields appropriately.
+
 -- Question 1: Write the structure of a query to create or replace a stored procedure called UPDATE_LEADERS_SCORE that takes a in_School_ID parameter as an integer and a in_Leader_Score parameter as an integer.
 
 DELIMITER //
